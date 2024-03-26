6ab3ca12116a95430ea62b5307e57ba4aeae3ae6
luca-masera <lucamasera1995@gmail.com>
2024-03-26 14:54:46 +0100

    create main + printIn("first project")

--- src/org/experis/java/HelloJava.java
+++ src/org/experis/java/HelloJava.java
@@ -3,5 +3,6 @@ package org.experis.java;
 public class HelloJava {
     public static void main(String[] args) {
         System.out.println("ciao");
+        System.out.println("Hello Java, first project");
     }
 }
